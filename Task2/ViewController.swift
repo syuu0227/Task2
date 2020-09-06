d232a6147da1c1eaf1b123e562de53614529e032
TakumaOgawa <syuu.2531@gmail.com>
2020-09-06 14:02:01 +0900

    課題2完成

--- Task2/ViewController.swift
+++ Task2/ViewController.swift
@@ -10,11 +10,45 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var numTextField1: UITextField!
+    @IBOutlet weak var numTextField2: UITextField!
+    @IBOutlet weak var ansLabel: UILabel!
+    @IBOutlet weak var selectSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        numTextField1.keyboardType = .numberPad
+        numTextField2.keyboardType = .numberPad
+        
     }
-
+    
+    
+    @IBAction func calculation(_ sender: Any) {
+        var ans = 0.0
+        let first = Double(self.numTextField1.text!) ?? 0.0
+        let second = Double(numTextField2.text!) ?? 0.0
+        
+        switch selectSegmentedControl.selectedSegmentIndex {
+        case 0:
+            ans = first + second
+        case 1:
+            ans = first - second
+        case 2:
+            ans = first * second
+        case 3:
+            if second == 0.0 {
+                ansLabel.text = "割る数には0以外を入力して下さい"
+                return
+            }
+            ans = first / second
+        default:
+            break
+        }
+        
+        ansLabel.text = String(ans)
+        
+    }
 
 }
 
